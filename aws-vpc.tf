2d413fdf5588d283c8779267896822025ca16af0
rohitpardasani <rpardasani@ine.com>
2025-03-27 16:50:26 -0400

    Base files

--- aws-vpc.tf
+++ aws-vpc.tf
@@ -12,3 +12,24 @@ resource "aws_vpc" "main" {
   }
 }
 
+resource "aws_subnet" "subnet_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Subnet-A"
+  }
+}
+
+resource "aws_subnet" "subnet_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Subnet-B"
+  }
+}
